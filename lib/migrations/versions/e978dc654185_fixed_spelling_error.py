8a0c398b2dd3daf9aa75ee039d32481b8adc8062
ckorir <chrisruto.k@gmail.com>
2023-12-08 14:48:00 +0300

    Completed assignment

---
+++ lib/migrations/versions/e978dc654185_fixed_spelling_error.py
@@ -0,0 +1,24 @@
+"""Fixed spelling error
+
+Revision ID: e978dc654185
+Revises: 93121ef0f3fa
+Create Date: 2023-12-08 14:41:29.104011
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e978dc654185'
+down_revision = '93121ef0f3fa'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
